45bff2701d2cc6aa1082f6f9db491278e46df47b
unknown <akhilkrishna7742@gmail.com>
2022-12-05 12:53:02 +0530

    firstcommit

---
+++ 1st day.py
@@ -0,0 +1,39 @@
+# parking lot with no count
+# nocars=0
+# nobikes=0
+# total=0
+# nocars=int(input("no of cars"))
+# nobikes=int(input("no of bikes"))
+# totalPayment=nobikes*20+nocars*40
+# print("total",totalPayment)
+
+#parking lot with count
+
+# bikes=0
+# cars=0
+# totalcount=0
+# bikes=int(input("no of bikes"))
+# cars=int(input("no of cars"))
+# totalcount=bikes*1+cars*1
+# print("totalcount",totalcount)
+
+
+# products count 
+
+# product1=10
+# product2=50
+# product3=40
+# total=0
+# productcount1=int(input("enter"))
+# productcount2=int(input("enter"))
+# productcount3=int(input("enter"))
+# total=productcount1*product1+productcount2*product2+productcount3*product3
+# print(total)
+
+
+# same using the if else condition
+
+
+
+
+
